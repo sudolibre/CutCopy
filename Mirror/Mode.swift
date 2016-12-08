b2ae277b39a865f145d8a20c133f7c0f1e2c1497
sudolibre <optout.breaker@gmail.com>
2016-12-07 20:17:59 -0500

    first draft

---
+++ Mirror/Mode.swift
@@ -0,0 +1,9 @@
+//
+//  Mode.swift
+//  Mirror
+//
+//  Created by Jonathon Day on 12/7/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+import Foundation
---
+++ MirrorKit/MirrorKit.h
@@ -0,0 +1,19 @@
+//
+//  MirrorKit.h
+//  MirrorKit
+//
+//  Created by Jonathon Day on 12/7/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MirrorKit.
+FOUNDATION_EXPORT double MirrorKitVersionNumber;
+
+//! Project version string for MirrorKit.
+FOUNDATION_EXPORT const unsigned char MirrorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MirrorKit/PublicHeader.h>
+
+
